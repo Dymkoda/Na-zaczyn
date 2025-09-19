c1c6ba0d9b8b7ca0281d0853ad6cb1b7fa4239c4
uczen <uczen@BARTOLOMEJ>
2025-09-19 15:55:53 +0200

    Została stworzona DataBase i dodana klasa Weather.comit

---
+++ DataBase/Weather.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace DataBase
+{
+    internal class Weather
+    {
+        public int Id { get; set; }
+        public string MeasyredBy { get; set; } = string.Empty;
+        public DateTime MeasyrementDate { get; set; }
+    }
+}
